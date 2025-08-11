1651db12df14feea6ef7a005b5fe193fd8d98f95
Visal285 <visal.von@student.cadt.edu.kh>
2025-08-11 11:55:07 +0700

    ex7

---
+++ week2/ex7.js
@@ -0,0 +1,13 @@
+function sumArray(arr){
+    if (arr.length === 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+console.log(sumArray([1, 2, 3]));
+console.log(sumArray([2, 2, 2, 2]));
+console.log(sumArray([50, 50, 1]));
