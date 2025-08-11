57219b38c8db878a5d482986243277903b9ac8c4
Visal285 <visal.von@student.cadt.edu.kh>
2025-08-11 11:44:58 +0700

    ex5

---
+++ week2/ex5.js
@@ -0,0 +1,11 @@
+ function isShortsWeather(temp){
+    if(temp>=35)
+        return true;
+    else
+        return false;
+    
+   
+ }
+console.log(isShortsWeather(40));
+console.log(isShortsWeather(22));
+console.log(isShortsWeather(37));
