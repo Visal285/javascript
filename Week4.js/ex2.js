9b255aa800717ca19952c1aa558c276a1d80abf4
Visal285 <visal.von@student.cadt.edu.kh>
2025-08-25 11:27:27 +0700

    ex2

---
+++ Week4.js/ex2.js
@@ -0,0 +1,9 @@
+let numbers = [5, 12, 8, 130, 44];
+let greaterThan10 = numbers.filter(num => num > 10);
+console.log(greaterThan10)
+
+let remove = numbers.splice(1,3);
+console.log(numbers);
+
+
+
